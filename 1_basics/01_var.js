5400314a98775b8196aa20bd754c6a9cff60298f
Mr-Shahrukh <147725808+Mr-Shahrukh@users.noreply.github.com>
2025-08-12 08:19:38 +0000

    var&Dtypes

---
+++ 1_basics/01_var.js
@@ -0,0 +1,15 @@
+const accId= 123
+let accEmail= "sam@gmail.com"
+var accPass="Sam123" //Prefer naot to usse Var
+accCity="Shirdi"
+let accState;
+
+// accId=3
+console.log(accId);
+accEmail="jack@gmail.com"
+console.log(accEmail);
+accPass="Sam1234"
+console.log(accPass);
+accCity="Mumbai"
+console.log(accCity);
+console.table([accId,accEmail,accPass,accCity,accState]) //it shows info in table format
