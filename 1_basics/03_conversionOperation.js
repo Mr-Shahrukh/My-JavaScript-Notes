5d7e17a2abed5aad8bff95ea685b4187c399b1f5
Mr-Shahrukh <147725808+Mr-Shahrukh@users.noreply.github.com>
2025-08-12 08:41:02 +0000

    conversion of datatypes

---
+++ 1_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score ="33q"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=>33
+//"33q" => NaN(Not a Number)
+//True =1; false = 0
+//Null=0
+//Undefinde=NaN
+
+let isLoggedIn =0
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//let isLoggedIn ="" =>false
+//let isLoggedIn ="sam" =>True
+
+let number = 33
+let stringNumber = String(number);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
